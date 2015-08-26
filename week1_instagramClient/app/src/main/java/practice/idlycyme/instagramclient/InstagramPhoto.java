df75a46a99ab6059bd8bb2bfeebb4e29888fbca0
YD LIN <cyme@yahoo-inc.com>
2015-08-26 22:57:56 +0800

    add pull-to-refresh

--- week1_instagramClient/app/src/main/java/practice/idlycyme/instagramclient/InstagramPhoto.java
+++ week1_instagramClient/app/src/main/java/practice/idlycyme/instagramclient/InstagramPhoto.java
@@ -1,5 +1,5 @@
 package practice.idlycyme.instagramclient;
-
+import android.text.format.DateUtils;
 public class InstagramPhoto {
     public String username;
     public String caption;
@@ -7,4 +7,8 @@ public class InstagramPhoto {
     public int imageHeight;
     public int likesCount;
     public InstagramUser user;
+    public long createdAtEpochSec;
+    public CharSequence getCreatedAtRelativeTimeSpan () {
+        return DateUtils.getRelativeTimeSpanString(createdAtEpochSec*1000, System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS);
+    }
 }
