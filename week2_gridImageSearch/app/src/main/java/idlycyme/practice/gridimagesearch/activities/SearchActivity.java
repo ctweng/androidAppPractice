d33277361c30c64cf964935e34cb2b458548951d
YD LIN <cyme@yahoo-inc.com>
2015-08-29 22:55:33 +0800

    add infinite loading & network check

--- week2_gridImageSearch/app/src/main/java/idlycyme/practice/gridimagesearch/activities/SearchActivity.java
+++ week2_gridImageSearch/app/src/main/java/idlycyme/practice/gridimagesearch/activities/SearchActivity.java
@@ -1,6 +1,9 @@
 package idlycyme.practice.gridimagesearch.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,6 +29,7 @@ import java.util.ArrayList;
 
 import idlycyme.practice.gridimagesearch.R;
 import idlycyme.practice.gridimagesearch.adapters.ImageResultsAdapter;
+import idlycyme.practice.gridimagesearch.libraries.EndlessScrollListener;
 import idlycyme.practice.gridimagesearch.models.ImageResult;
 
 public class SearchActivity extends AppCompatActivity {
@@ -34,6 +38,10 @@ public class SearchActivity extends AppCompatActivity {
     private AsyncHttpClient apiClient;
     private ArrayList<ImageResult> imageResults;
     private ImageResultsAdapter aImageResults;
+    private String nextSearchText;
+    private int offestIncrement = 8;
+    private int maxSearchOffset;
+    private static final String searchURL = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +52,12 @@ public class SearchActivity extends AppCompatActivity {
         imageResults = new ArrayList<ImageResult>();
         aImageResults = new ImageResultsAdapter(this, this.imageResults);
         gvResults.setAdapter(this.aImageResults);
+        gvResults.setOnScrollListener(new EndlessScrollListener() {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount) {
+                fetchSearchResults(nextSearchText, (page-1)*offestIncrement);
+            }
+        });
 ;
     }
 
@@ -53,6 +67,10 @@ public class SearchActivity extends AppCompatActivity {
         gvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (!isNetworkAvailable()) {
+                    Toast.makeText(getBaseContext(), "Network unavailable, try later!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 Intent result2detail = new Intent(SearchActivity.this, ImageDisplayActivity.class);
                 ImageResult result = imageResults.get(position);
                 result2detail.putExtra("result", result);
@@ -83,19 +101,34 @@ public class SearchActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onImageSearch(View v) {
-        String query = this.etQuery.getText().toString();
-        String searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + query + "&rsz=8";
-        Log.d("url is ", searchUrl);
-        apiClient.get(searchUrl, null, new JsonHttpResponseHandler() {
+    private void fetchSearchResults(String searchText, int nextSearchOffset) {
+        if (!isNetworkAvailable()) {
+            Toast.makeText(this, "Network unavailable, try later!", Toast.LENGTH_LONG).show();
+        }
+
+        if (!searchText.equals(nextSearchText)) {
+            resetSearchParams();
+            aImageResults.clear();
+            nextSearchText = searchText;
+        } else if (maxSearchOffset != -1 && maxSearchOffset < nextSearchOffset) {
+            Toast.makeText(this, "No more results!", Toast.LENGTH_SHORT).show();
+            resetSearchParams();
+            return;
+        }
+
+        RequestParams params = new RequestParams();
+        params.put("q", nextSearchText);
+        params.put("rsz", offestIncrement);
+        params.put("start", nextSearchOffset);
+        apiClient.get(searchURL, params, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 JSONArray imageResultsJson = null;
                 try {
-                    Log.i("aaaa", response.toString());
+                    Log.i("cursor", response.getJSONObject("responseData").getJSONObject("cursor").toString());
+                    JSONArray pages = response.getJSONObject("responseData").getJSONObject("cursor").getJSONArray("pages");
+                    maxSearchOffset = Integer.valueOf(pages.getJSONObject(pages.length()-1).getString("start"));
                     imageResultsJson = response.getJSONObject("responseData").getJSONArray("results");
-                    imageResults.clear();
-
                     imageResults.addAll(ImageResult.fromJSONArray(imageResultsJson));
                     aImageResults.notifyDataSetChanged();
 
@@ -105,7 +138,6 @@ public class SearchActivity extends AppCompatActivity {
                 }
 
             }
-
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
                 super.onFailure(statusCode, headers, responseString, throwable);
@@ -113,6 +145,26 @@ public class SearchActivity extends AppCompatActivity {
                 Log.d("client failure", responseString + "           " + String.valueOf(statusCode) + "          " + headers.toString());
             }
         });
-        //Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onImageSearch(View v) {
+        String query = this.etQuery.getText().toString();
+        if (query.equals("")) {
+            Toast.makeText(this, "Please enter something to search", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        fetchSearchResults(query, 0);
+    }
+
+    private Boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
+
+    private void resetSearchParams() {
+        nextSearchText = "";
+        maxSearchOffset = -1;
     }
 }
